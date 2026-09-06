9cd53a18497ffb9ae2b1d9f5a1d9273e23e96f8d
twagley <twagley@gnspes.ca>
2026-09-06 15:12:25 -0300

    Update criteria.js

--- criteria.js
+++ criteria.js
@@ -900,7 +900,7 @@ const STYLE_OBJ = {
 		criteria:	"author",
 		question: 	"Have you written your name as the author?",
 		example:	"<meta name='author' content='Dave Drapak'>",
-		//reference:	null,
+		reference:	REFERENCE_OBJ.HTMLBasics,
 		negative:	false,
 		begin:		"name.*=.*author",
 		end:		"content="
@@ -916,7 +916,7 @@ const STYLE_OBJ = {
 					"		Finished:	18. September 2018	- Drapak\n" +
 					"-->",
 
-		reference:	REFERENCE_OBJ.none,
+		reference:	REFERENCE_OBJ.OTHERFilename,
 		negative:	false,
 		begin:		"[<][!]--",
 		end:		"--[>]"
@@ -956,7 +956,7 @@ const STYLE_OBJ = {
 		criteria:	"filename",
 		question: 	"Have you correctly added your name to the filename?",
 		example:	"1.05S-HelloColor-Drapak.html",
-		reference:	REFERENCE_OBJ.none,
+		reference:	REFERENCE_OBJ.OTHERFilename,
 		negative:	false,
 		begin:		"FILENAME:",
 	},
@@ -1077,7 +1077,7 @@ const STYLE_OBJ = {
 		criteria:	"style",
 		question: 	"Have you included a style block in your HTML page?",
 		example:	"<style></style>",
-		reference:	REFERENCE_OBJ.none,
+		reference:	REFERENCE_OBJ.HTMLBasics,
 		negative:	false,
 		begin:		"<style",
 		end:		"</style>"
